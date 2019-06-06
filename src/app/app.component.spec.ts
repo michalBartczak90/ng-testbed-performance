52dcbd26a884523c9a5a2f9589011519b8413525
Michał Bartczak <michal.bartczak.90@gmail.com>
2019-06-06 22:23:30 +0200

    configureTestBedWithSharedModuleCompilationResults

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+
+import { configureTestBedWithSharedModuleCompilationResults } from '../test';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  configureTestBedWithSharedModuleCompilationResults(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -11,25 +13,41 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    });
   });
 
-  it(`should have as title 'ng-testbed-performance'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-testbed-performance');
-  });
+  // SLOW VERSION
+  // beforeEach(done => (async () => {
+  //   TestBed.configureTestingModule({
+  //     imports: [
+  //       RouterTestingModule
+  //     ],
+  //     declarations: [
+  //       AppComponent
+  //     ],
+  //   });
+  //
+  //   await TestBed.compileComponents();
+  // })().then(done).catch(done.fail));
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-testbed-performance!');
-  });
+  for (let i = 0; i < 100; i++) {
+    it('should create the app', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it(`should have as title 'ng-testbed-performance'`, () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.title).toEqual('ng-testbed-performance');
+    });
+
+    it(`${i}: should render title in a h1 tag`, () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-testbed-performance!');
+    });
+  }
 });
--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,38 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/zone-testing';
-import { getTestBed } from '@angular/core/testing';
+import {ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
+
+export function configureTestBedWithSharedModuleCompilationResults(configurationFactory: () => void) {
+  const testBedApi: any = getTestBed();
+  const originReset = TestBed.resetTestingModule;
+
+  beforeAll(() => {
+    TestBed.resetTestingModule();
+    TestBed.resetTestingModule = () => TestBed;
+  });
+
+  afterEach(() => {
+    testBedApi._activeFixtures.forEach((fixture: ComponentFixture<any>) => fixture.destroy());
+    testBedApi._instantiated = false;
+  });
+
+  afterAll(() => {
+    TestBed.resetTestingModule = originReset;
+    TestBed.resetTestingModule();
+  });
+
+  beforeAll(done => (async () => {
+    configurationFactory();
+    await TestBed.compileComponents();
+  })().then(done).catch(done.fail));
+}
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
